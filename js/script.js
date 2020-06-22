cce6b6d084e20b82dace4e0cbc4b42969194a440
Roman <www.tolkoya@gmail.com>
2020-06-22 11:31:44 +0300

    Выполнено 4 задания
    Список фильмов расставлен в алфавитном порядке

--- js/script.js
+++ js/script.js
@@ -34,3 +34,12 @@ const advertisingImages = advertisingBlock.getElementsByTagName('img');
 document.querySelector('.promo__genre').textContent = 'Драма';
 
 document.querySelector('.promo__bg').style.backgroundImage = 'url(img/bg.jpg)';
+
+let filmsList = document.querySelector('.promo__interactive-list');
+let filmsItems = filmsList.querySelectorAll('.promo__interactive-item');
+
+movieDB.movies.sort();
+
+[...filmsItems].forEach((item, i) => {
+  item.textContent = movieDB.movies[i];
+});
